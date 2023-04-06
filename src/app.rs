6dd14b9fe2a74277c7c5cab27953bed7acb70901
zshehov <z.shehov@gmail.com>
2023-04-06 18:55:56 +0300

    initial tab management

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,115 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct ItsLogicalApp {
-    // Example stuff:
-    label: String,
+    ask_tab: AskTab,
+    term_tabs: Vec<TermTab>,
+    current_tab: TabKind,
+    terms: Vec<Term>,
+}
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+struct Term {
+    name: String
+}
+
+struct AskTab {}
+impl Tab for AskTab {
+    fn name(&self) -> String {
+        "Ask".to_owned()
+    }
+
+    fn show(&self, ui: &mut egui::Ui) {
+        ui.heading("Ask a question");
+        ui.separator();
+    }
+}
+
+struct TermTab {
+    name: String,
+}
+
+impl Tab for TermTab {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn show(&self, ui: &mut egui::Ui) {
+        ui.heading(&self.name);
+        ui.separator();
+
+        egui::ScrollArea::vertical()
+            .id_source("description_scroll_area")
+            .show(ui, |ui| {
+                ui.with_layout(
+                    egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
+                    |ui| {
+                        ui.label(
+                            egui::RichText::new("A mother is a parent that is female")
+                                .italics()
+                                .small(),
+                        )
+                    },
+                )
+            });
+        ui.separator();
+        // Rules:
+        egui::ScrollArea::vertical()
+            .id_source("rules_scroll_area")
+            .show(ui, |ui| {
+                ui.with_layout(
+                    egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
+                    |ui| {
+                        ui.label("mother(X,Y) if parent(X,Y) and female(X)");
+                        ui.label("mother(_,_) if _________________________ +");
+                    },
+                )
+            });
+        ui.separator();
+        // Facts:
+        egui::ScrollArea::vertical()
+            .id_source("facts_scroll_area")
+            .show(ui, |ui| {
+                ui.with_layout(
+                    egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
+                    |ui| {
+                        ui.label("mother(amy,steve)");
+                        ui.label("mother(kunka,mitko)");
+                        ui.label("mother(_,_) +");
+                    },
+                )
+            });
+        ui.separator();
+        // Reffered by:
+        egui::ScrollArea::vertical()
+            .id_source("referred_by_scroll_area")
+            .show(ui, |ui| {
+                ui.with_layout(
+                    egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
+                    |ui| {
+                        ui.label("Referred by:");
+                        ui.label("grandmother");
+                    },
+                )
+            });
+    }
+}
+
+#[derive(PartialEq)]
+enum TabKind {
+    Ask,
+    Term(usize),
+}
+
+trait Tab {
+    fn name(&self) -> String;
+    fn show(&self, ui: &mut egui::Ui);
 }
 
 impl Default for ItsLogicalApp {
     fn default() -> Self {
-        Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
+        return Self {
+            ask_tab: AskTab {},
+            term_tabs: vec![],
+            current_tab: TabKind::Ask,
+            terms: vec![Term{name: "mother".to_owned()}, Term{name: "father".to_owned()}],
+        };
     }
 }
 
@@ -25,26 +118,20 @@ impl ItsLogicalApp {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
         Default::default()
     }
 }
 
 impl eframe::App for ItsLogicalApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            ask_tab,
+            term_tabs,
+            current_tab,
+            terms,
+        } = self;
 
         egui::SidePanel::left("terms_panel").show(ctx, |ui| {
             ui.heading("Terms");
@@ -52,70 +139,36 @@ impl eframe::App for ItsLogicalApp {
 
             let scroll_area = egui::ScrollArea::vertical().auto_shrink([false; 2]);
             scroll_area.show(ui, |ui| {
-                // TODO: Add actual terms from the DB
-                for item in 1..=50 {
-                    ui.label(format!("this is item: {}", item));
+                for term in &self.terms {
+                    if ui.small_button(&term.name).clicked() {
+                        self.term_tabs.push(TermTab { name: term.name.to_owned() })
+                    }
                 }
             })
         });
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Mother");
+            ui.horizontal(|ui| {
+                ui.selectable_value(
+                    &mut self.current_tab,
+                    TabKind::Ask,
+                    egui::RichText::new(self.ask_tab.name()).strong(),
+                );
+                ui.separator();
+
+                for (i, tab) in self.term_tabs.iter().enumerate() {
+                    ui.selectable_value(&mut self.current_tab, TabKind::Term(i), tab.name());
+                }
+            });
             ui.separator();
 
-            egui::ScrollArea::vertical()
-                .id_source("description_scroll_area")
-                .show(ui, |ui| {
-                    ui.with_layout(
-                        egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
-                        |ui| {
-                            ui.label(
-                                egui::RichText::new("A mother is a parent that is female")
-                                    .italics()
-                                    .small(),
-                            )
-                        },
-                    )
-                });
-            ui.separator();
-            // Rules:
-            egui::ScrollArea::vertical()
-                .id_source("rules_scroll_area")
-                .show(ui, |ui| {
-                    ui.with_layout(
-                        egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
-                        |ui| {
-                            ui.label("mother(X,Y) if parent(X,Y) and female(X)");
-                            ui.label("mother(_,_) if _________________________ +");
-                        },
-                    )
-                });
-            ui.separator();
-            // Facts:
-            egui::ScrollArea::vertical()
-                .id_source("facts_scroll_area")
-                .show(ui, |ui| {
-                    ui.with_layout(
-                        egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
-                        |ui| {
-                            ui.label("mother(amy,steve)");
-                            ui.label("mother(kunka,mitko)");
-                            ui.label("mother(_,_) +");
-                        },
-                    )
-                });
-            ui.separator();
-            // Reffered by:
-            egui::ScrollArea::vertical()
-                .id_source("referred_by_scroll_area")
-                .show(ui, |ui| {
-                    ui.with_layout(
-                        egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
-                        |ui| {
-                            ui.label("Referred by:");
-                            ui.label("grandmother");
-                        },
-                    )
-                });
+            match self.current_tab {
+                TabKind::Term(idx) => {
+                    self.term_tabs.get(idx).unwrap().show(ui);
+                }
+                TabKind::Ask => {
+                    self.ask_tab.show(ui)
+                }
+            }
         });
 
         if false {
