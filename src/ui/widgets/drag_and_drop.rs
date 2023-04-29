a04fa8bd804c6af16f5b82e4cf7b6706d49358b3
Zdravko Shehov <z.shehov@gmail.com>
2023-04-29 19:49:32 +0300

    fix bottoms each run

--- src/ui/widgets/drag_and_drop.rs
+++ src/ui/widgets/drag_and_drop.rs
@@ -3,14 +3,9 @@ use std::cmp::min;
 use eframe::epaint::RectShape;
 use egui::{CursorIcon, Id, LayerId, Order, Rect, Sense, Shape, Ui, Vec2};
 
-enum Bottoms {
-    Unknown(Vec<f32>),
-    Known(Vec<f32>),
-}
-
 pub(crate) struct DragAndDrop {
     pub(crate) items: Vec<String>,
-    bottoms: Bottoms,
+    bottoms: Vec<f32>,
 }
 
 impl DragAndDrop {
@@ -18,7 +13,7 @@ impl DragAndDrop {
         let items_len = items.len();
         Self {
             items,
-            bottoms: Bottoms::Unknown(vec![0.0; items_len]),
+            bottoms: vec![0.0; items_len],
         }
     }
 
@@ -33,77 +28,64 @@ impl DragAndDrop {
         let mut content_ui = ui.child_ui(inner_rect, *ui.layout());
 
         content_ui.vertical(|ui| {
-            match &mut self.bottoms {
-                Bottoms::Unknown(bottoms) => {
-                    // first iteration is only to get familiar with the bottoms
-                    for (idx, item) in self.items.iter().enumerate() {
-                        let response = ui.scope(|ui| ui.label(item)).response;
-                        bottoms[idx] = response.rect.bottom();
-                    }
-                    self.bottoms = Bottoms::Known(bottoms.to_owned());
+            let mut dragged_item: Option<(usize, String)> = None;
+            for (idx, item) in self.items.iter().enumerate() {
+                let item_id = Id::new(id_source).with(item.to_owned());
+                if ui.memory(|mem| mem.is_being_dragged(item_id)) {
+                    dragged_item = Some((idx, item.to_owned()));
                 }
-                Bottoms::Known(bottoms) => {
-                    let mut dragged_item: Option<(usize, String)> = None;
-                    for (idx, item) in self.items.iter().enumerate() {
-                        let item_id = Id::new(id_source).with(item.to_owned());
-                        if ui.memory(|mem| mem.is_being_dragged(item_id)) {
-                            dragged_item = Some((idx, item.to_owned()));
-                        }
-                    }
+            }
 
-                    if let Some((current_dragged_ix, dragged_item)) = dragged_item {
-                        if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
-                            match bottoms
-                                .binary_search_by(|x| x.partial_cmp(&pointer_pos.y).unwrap())
-                            {
-                                Ok(dragged_new_idx) => {
-                                    let dragged_new_idx =
-                                        min(dragged_new_idx, self.items.len() - 1);
-                                    if dragged_new_idx != current_dragged_ix {
-                                        self.items.remove(current_dragged_ix);
-                                        self.items.insert(dragged_new_idx, dragged_item.to_owned());
-                                    }
-                                }
-                                Err(dragged_new_idx) => {
-                                    let dragged_new_idx =
-                                        min(dragged_new_idx, self.items.len() - 1);
-                                    if dragged_new_idx != current_dragged_ix {
-                                        self.items.remove(current_dragged_ix);
-                                        self.items.insert(dragged_new_idx, dragged_item.to_owned());
-                                    }
-                                }
+            if let Some((current_dragged_ix, dragged_item)) = dragged_item {
+                // there can't be a dragged item on the first frame, so this will never get
+                // executed with bottoms of 0s
+                if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
+                    match self
+                        .bottoms
+                        .binary_search_by(|x| x.partial_cmp(&pointer_pos.y).unwrap())
+                    {
+                        Ok(dragged_new_idx) => {
+                            let dragged_new_idx = min(dragged_new_idx, self.items.len() - 1);
+                            if dragged_new_idx != current_dragged_ix {
+                                self.items.remove(current_dragged_ix);
+                                self.items.insert(dragged_new_idx, dragged_item.to_owned());
+                            }
+                        }
+                        Err(dragged_new_idx) => {
+                            let dragged_new_idx = min(dragged_new_idx, self.items.len() - 1);
+                            if dragged_new_idx != current_dragged_ix {
+                                self.items.remove(current_dragged_ix);
+                                self.items.insert(dragged_new_idx, dragged_item.to_owned());
                             }
                         }
                     }
-                    for (item, bottom) in self.items.iter().zip(bottoms.iter_mut()) {
-                        let item_id = Id::new(id_source).with(item.to_owned());
+                }
+            }
+            for (item, bottom) in self.items.iter().zip(self.bottoms.iter_mut()) {
+                let item_id = Id::new(id_source).with(item.to_owned());
 
-                        let is_being_dragged = ui.memory(|mem| mem.is_being_dragged(item_id));
+                let is_being_dragged = ui.memory(|mem| mem.is_being_dragged(item_id));
 
-                        if !is_being_dragged {
-                            let response = ui.scope(|ui| ui.label(item)).response;
-                            *bottom = response.rect.bottom();
+                if !is_being_dragged {
+                    let response = ui.scope(|ui| ui.label(item)).response;
+                    *bottom = response.rect.bottom();
 
-                            // Check for drags:
-                            let response = ui.interact(response.rect, item_id, Sense::drag());
-                            if response.hovered() {
-                                ui.ctx().set_cursor_icon(CursorIcon::Grab);
-                            }
-                        } else {
-                            ui.ctx().set_cursor_icon(CursorIcon::Grabbing);
+                    // Check for drags:
+                    let response = ui.interact(response.rect, item_id, Sense::drag());
+                    if response.hovered() {
+                        ui.ctx().set_cursor_icon(CursorIcon::Grab);
+                    }
+                } else {
+                    ui.ctx().set_cursor_icon(CursorIcon::Grabbing);
 
-                            // Paint the body to a new layer:
-                            let layer_id = LayerId::new(Order::Tooltip, item_id);
-                            let response = ui
-                                .with_layer_id(layer_id, |ui| ui.label(item))
-                                .response;
-                            *bottom = response.rect.bottom();
+                    // Paint the body to a new layer:
+                    let layer_id = LayerId::new(Order::Tooltip, item_id);
+                    let response = ui.with_layer_id(layer_id, |ui| ui.label(item)).response;
+                    *bottom = response.rect.bottom();
 
-                            if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
-                                let delta = pointer_pos - response.rect.center();
-                                ui.ctx().translate_layer(layer_id, delta);
-                            }
-                        }
+                    if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
+                        let delta = pointer_pos - response.rect.center();
+                        ui.ctx().translate_layer(layer_id, delta);
                     }
                 }
             }
