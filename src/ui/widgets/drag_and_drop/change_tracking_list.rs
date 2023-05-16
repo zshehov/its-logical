9475606ed7f48ee8657139825548a1b629f6c9a5
Zdravko Shehov <z.shehov@gmail.com>
2023-05-16 15:57:47 +0300

    add change tracking list

---
+++ src/ui/widgets/drag_and_drop/change_tracking_list.rs
@@ -0,0 +1,201 @@
+use std::{collections::HashMap, hash::Hash};
+
+#[derive(PartialEq, Debug)]
+pub(crate) enum Change<T> {
+    None,
+    Pushed(),
+    Moved(Vec<usize>),
+    Removed(usize, T),
+}
+
+pub(crate) struct ChangeTrackingVec<T: Clone + Eq + Hash> {
+    current_changes: Vec<Change<T>>,
+    items: Vec<T>,
+    order_changes: Vec<usize>,
+    order_has_changed: bool,
+}
+
+impl<T: Clone + Eq + Hash> ChangeTrackingVec<T> {
+    pub(crate) fn new(items: Vec<T>) -> Self {
+        let mut order_changes = Vec::<usize>::with_capacity(items.len());
+
+        for idx in 0..items.len() {
+            order_changes.push(idx);
+        }
+
+        Self {
+            current_changes: vec![],
+            order_changes,
+            items,
+            order_has_changed: false,
+        }
+    }
+
+    pub(crate) fn push(&mut self, item: T) {
+        if let Some(order_changes) = self.flush_order_changes() {
+            self.current_changes.push(Change::Moved(order_changes));
+        }
+        self.items.push(item.clone());
+        self.order_changes.push(self.items.len() - 1);
+        self.current_changes.push(Change::Pushed());
+    }
+
+    pub(crate) fn move_item(&mut self, from: usize, to: usize) {
+        let item = self.items.remove(from);
+        self.items.insert(to, item);
+        if from > to {
+            self.order_changes[to] = from;
+            for order_idx in &mut self.order_changes[to + 1..=from] {
+                *order_idx -= 1;
+            }
+        } else {
+            self.order_changes[to] = from;
+            for order_idx in &mut self.order_changes[from..to] {
+                *order_idx += 1;
+            }
+        }
+        self.order_has_changed = true;
+    }
+
+    pub(crate) fn remove(&mut self, idx: usize) {
+        if let Some(order_changes) = self.flush_order_changes() {
+            self.current_changes.push(Change::Moved(order_changes));
+        }
+        let removed = self.items.remove(idx);
+        self.order_changes.remove(idx);
+        // order changes before the removal have already been persisted
+        self.reset_order();
+        self.current_changes.push(Change::Removed(idx, removed));
+    }
+
+    pub(crate) fn iter(&self) -> std::slice::Iter<'_, T> {
+        self.items.iter()
+    }
+
+    pub(crate) fn iter_mut(&mut self) -> std::slice::IterMut<'_, T> {
+        self.items.iter_mut()
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    pub(crate) fn get_current_changes(&mut self) -> Vec<Change<T>> {
+        let mut empty_changes: Vec<Change<T>> = vec![];
+        std::mem::swap(&mut self.current_changes, &mut empty_changes);
+        empty_changes
+    }
+
+    fn flush_order_changes(&mut self) -> Option<Vec<usize>> {
+        if self.order_has_changed {
+            let mut order_changes = Vec::<usize>::with_capacity(self.items.len());
+
+            for pre_change_idx in &self.order_changes {
+                order_changes.push(*pre_change_idx);
+            }
+
+            self.reset_order();
+            self.order_has_changed = false;
+            return Some(order_changes);
+        }
+        None
+    }
+
+    fn reset_order(&mut self) {
+        let mut idx = 0;
+        for order_idx in &mut self.order_changes {
+            *order_idx = idx;
+            idx += 1;
+        }
+    }
+}
+
+#[test]
+fn test_new() {
+    let v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    assert_eq!(v.items, vec![1, 2, 3]);
+    assert_eq!(v.order_changes.len(), 3);
+    assert_eq!(v.current_changes.len(), 0);
+}
+
+#[test]
+fn test_push() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    v.push(4);
+    assert_eq!(v.items, vec![1, 2, 3, 4]);
+    assert_eq!(v.order_changes.len(), 4);
+    assert_eq!(v.current_changes.len(), 1);
+    assert_eq!(v.current_changes[0], Change::Pushed());
+}
+
+#[test]
+fn test_move_item() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    v.move_item(0, 2);
+    assert_eq!(v.items, vec![2, 3, 1]);
+}
+
+#[test]
+fn test_remove() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    v.remove(1);
+    assert_eq!(v.items, vec![1, 3]);
+    assert_eq!(v.order_changes.len(), 2);
+    assert_eq!(v.current_changes.len(), 1);
+    assert_eq!(v.current_changes[0], Change::Removed(1, 2));
+}
+
+#[test]
+fn test_flush_order_changes() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    v.move_item(0, 2);
+    assert_eq!(v.items, vec![2, 3, 1]);
+    assert_eq!(v.order_has_changed, true);
+    let order_changes = v.flush_order_changes().unwrap();
+    assert_eq!(order_changes, vec![1, 2, 0]);
+    assert_eq!(v.order_has_changed, false);
+    assert_eq!(v.order_changes.len(), 3);
+}
+
+#[test]
+fn test_push_after_move() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    v.move_item(0, 2);
+    assert_eq!(v.items, vec![2, 3, 1]);
+    v.push(69);
+    assert_eq!(v.items, vec![2, 3, 1, 69]);
+    assert_eq!(v.order_changes.len(), 4);
+    let current_changes = v.get_current_changes();
+    assert_eq!(current_changes.len(), 2);
+    assert_eq!(current_changes[0], Change::Moved(vec![1, 2, 0]));
+    assert_eq!(current_changes[1], Change::Pushed());
+}
+
+#[test]
+fn test_remove_after_move() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    v.move_item(0, 2);
+    assert_eq!(v.items, vec![2, 3, 1]);
+    v.remove(1);
+    assert_eq!(v.items, vec![2, 1]);
+    assert_eq!(v.order_changes.len(), 2);
+    let current_changes = v.get_current_changes();
+    assert_eq!(current_changes.len(), 2);
+    assert_eq!(current_changes[0], Change::Moved(vec![1, 2, 0]));
+    assert_eq!(current_changes[1], Change::Removed(1, 3));
+}
+
+#[test]
+fn test_iter() {
+    let v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    let new_vec: Vec<&i32> = v.iter().collect();
+    assert_eq!(new_vec, vec![&1, &2, &3]);
+}
+
+#[test]
+fn test_iter_mut() {
+    let mut v = ChangeTrackingVec::new(vec![1, 2, 3]);
+    let mut new_vec: Vec<&mut i32> = v.iter_mut().collect();
+    *new_vec[1] = 69;
+    assert_eq!(v.items, vec![1, 69, 3]);
+}
--- src/ui/widgets/drag_and_drop/mod.rs
+++ src/ui/widgets/drag_and_drop/mod.rs
@@ -1,36 +1,41 @@
-use std::cmp::min;
+use std::{cmp::min, hash::Hash};
 
 use eframe::epaint::RectShape;
 use egui::{CursorIcon, Id, LayerId, Order, Rect, Sense, Shape, Ui, Vec2};
 
-pub(crate) trait ListUniqueID {
-    fn id(&self) -> String;
-}
+use self::change_tracking_list::{Change, ChangeTrackingVec};
 
-pub(crate) struct DragAndDrop<T: ListUniqueID + Clone> {
+pub(crate) mod change_tracking_list;
+
+pub(crate) struct DragAndDrop<T: Hash + Clone + Eq> {
     active: bool,
-    items: Vec<T>,
+    items: ChangeTrackingVec<T>,
     create_item: Box<dyn Fn() -> T>,
     bottoms: Vec<f32>,
     default_value_id: Id,
 }
 const ID_SOURCE: &str = "drag_and_drop";
 
-impl<T: ListUniqueID + Clone> DragAndDrop<T> {
+impl<T: Hash + Clone + Eq> DragAndDrop<T> {
     // transfering ownership of the Vec to the DragAndDrop
     pub(crate) fn new(items: Vec<T>, create_item: Box<dyn Fn() -> T>) -> Self {
         let items_len = items.len();
         let prototype = create_item();
         Self {
             active: false,
-            items,
+            items: ChangeTrackingVec::new(items),
             create_item,
             bottoms: vec![0.0; items_len],
-            default_value_id: Id::new(ID_SOURCE).with(prototype.id()),
+            default_value_id: Id::new(ID_SOURCE).with(prototype),
         }
     }
-    pub(crate) fn set_active(&mut self, active: bool) {
-        self.active = active;
+    pub(crate) fn set_active(&mut self) {
+        self.active = true;
+    }
+
+    pub(crate) fn set_inactive(&mut self) -> Vec<Change<T>> {
+        self.active = false;
+        return self.items.get_current_changes();
     }
 
     pub(crate) fn show(&mut self, ui: &mut Ui, show_item: impl Fn(&mut T, &mut Ui)) {
@@ -44,39 +49,31 @@ impl<T: ListUniqueID + Clone> DragAndDrop<T> {
             let mut content_ui = ui.child_ui(inner_rect, *ui.layout());
 
             content_ui.vertical(|ui| {
-                let mut dragged_item: Option<(usize, T)> = None;
+                let mut dragged_item: Option<usize> = None;
                 for (idx, item) in self.items.iter().enumerate() {
-                    let item_id = Id::new(ID_SOURCE).with(item.id());
+                    let item_id = Id::new(ID_SOURCE).with(item);
                     if ui.memory(|mem| mem.is_being_dragged(item_id)) {
-                        dragged_item = Some((idx, item.to_owned()));
+                        dragged_item = Some(idx);
                     }
                 }
 
-                if let Some((current_dragged_ix, dragged_item)) = dragged_item {
+                // move the dragged item in the list position relevant to its Y position
+                if let Some(dragged_current_idx) = dragged_item {
                     // there can't be a dragged item on the first frame, so this will never get
                     // executed with bottoms of 0s
                     if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
                         if self.active {
-                            match self
+                            let dragged_new_idx = match self
                                 .bottoms
                                 .binary_search_by(|x| x.partial_cmp(&pointer_pos.y).unwrap())
                             {
-                                Ok(dragged_new_idx) => {
-                                    let dragged_new_idx =
-                                        min(dragged_new_idx, self.items.len() - 1);
-                                    if dragged_new_idx != current_dragged_ix {
-                                        self.items.remove(current_dragged_ix);
-                                        self.items.insert(dragged_new_idx, dragged_item.to_owned());
-                                    }
-                                }
-                                Err(dragged_new_idx) => {
-                                    let dragged_new_idx =
-                                        min(dragged_new_idx, self.items.len() - 1);
-                                    if dragged_new_idx != current_dragged_ix {
-                                        self.items.remove(current_dragged_ix);
-                                        self.items.insert(dragged_new_idx, dragged_item.to_owned());
-                                    }
-                                }
+                                Ok(dragged_new_idx) => dragged_new_idx,
+                                Err(dragged_new_idx) => dragged_new_idx,
+                            };
+
+                            let dragged_new_idx = min(dragged_new_idx, self.items.len() - 1);
+                            if dragged_new_idx != dragged_current_idx {
+                                self.items.move_item(dragged_current_idx, dragged_new_idx)
                             }
                         }
                     }
@@ -93,7 +90,7 @@ impl<T: ListUniqueID + Clone> DragAndDrop<T> {
                         show_item(item, ui);
                         continue;
                     }
-                    let item_id = Id::new(ID_SOURCE).with(item.id());
+                    let item_id = Id::new(ID_SOURCE).with(&item);
                     if item_id == self.default_value_id {
                         default_item_present = true;
                     }
@@ -147,18 +144,16 @@ impl<T: ListUniqueID + Clone> DragAndDrop<T> {
                     self.items.remove(item_for_deletion_idx);
                     self.bottoms.pop();
                 }
-                if self.active {
-                    ui.shrink_width_to_current();
-                    ui.separator();
-                    ui.vertical_centered(|ui| {
-                        if !default_item_present {
-                            if ui.button("+").clicked() {
-                                self.items.push((self.create_item)());
-                                self.bottoms.push(0.0);
-                            }
+                ui.shrink_width_to_current();
+                ui.separator();
+                ui.vertical_centered(|ui| {
+                    if !default_item_present {
+                        if ui.button("+").clicked() {
+                            self.items.push((self.create_item)());
+                            self.bottoms.push(0.0);
                         }
-                    });
-                }
+                    }
+                });
             });
 
             let outer_rect =
