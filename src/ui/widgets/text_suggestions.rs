3ed5e43f8fef38d4104b88e51411a6ba259a832c
Zdravko Shehov <z.shehov@gmail.com>
2023-07-22 19:08:56 +0300

    focus ergonomics

--- src/ui/widgets/text_suggestions.rs
+++ src/ui/widgets/text_suggestions.rs
@@ -33,16 +33,36 @@ impl SuggestionsPopup {
         egui::popup_below_widget(ui, id, &response, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
                 // TODO: cache the filtered response
-                for s in suggestions.filter(value) {
+                let mut last_lost_focus = false;
+                for (idx, s) in suggestions.filter(value).enumerate() {
+                    last_lost_focus = false;
                     let suggestion = s.value();
 
-                    if s.show(ui).clicked() {
+                    let suggestion_response = s.show(ui);
+                    if idx == 0 {
+                        // Just ergonimocs - focus is not moved correctly when jumping between the
+                        // edit_box and the first suggested element. Forced this here
+                        if response.lost_focus() {
+                            suggestion_response.request_focus();
+                        } else if suggestion_response.lost_focus()
+                            && ui.input(|i| i.modifiers.shift)
+                        {
+                            response.request_focus();
+                        }
+                    }
+                    if suggestion_response.lost_focus() {
+                        last_lost_focus = true;
+                    }
+                    if suggestion_response.clicked() {
                         *value = suggestion;
                         changed = true;
                         ui.memory_mut(|m| m.close_popup());
                         response.request_focus();
                         break;
                     }
+                }
+                if last_lost_focus {
+                    ui.memory_mut(|m| m.close_popup());
                 }
             });
         });
